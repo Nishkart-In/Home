6f62caa0fa7c4110d8aa709e890ccd331b0bf757
Nishkart-In <workwithdevvv@gmail.com>
2025-10-02 13:44:40 +0530

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-
-/**
- * Rename this file to config.js after filling values.
- */
-window.NKCFG = {
-  SUPABASE_URL: "https://cxvmhqbtpqjnuuklxlma.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4dm1ocWJ0cHFqbnV1a2x4bG1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzNDYyMjMsImV4cCI6MjA3NDkyMjIyM30.IlhVCBBivCcGHdxbIU71tIfyiNoIVHTv_xQ1wA03Jgc",
-  WA_NUMBER: "919711947389" // your WhatsApp number (no + sign)
-};
